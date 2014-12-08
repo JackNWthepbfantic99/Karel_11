7b4452bfa15eaaae0dd9bbfafef2320e419b551c
JackNWthepbfantic99 <staceynavin@Jacks-MacBook-Air-2.local>
2014-12-08 13:12:40 +0000

    My Student ID is displayed

--- Lab11.java
+++ Lab11.java
@@ -1,17 +1,34 @@
 /**
  * An algorithm helps an Athlete solve a maze, perhaps by keeping one hand against a wall.
  * 
- * @author <...>
- * @version <...>
+ * @author jack Navin-Weinstein
+ * @version Mon 8 Dec>
  *
  */
 
 import edu.fcps.karel2.Display; 
-
+import edu.fcps.Digit;
 public class Lab11 {
 	 
-	 public static void main(String[] args) {
 	
+	  public static void main(String[] args) {
+    Display.openWorld("maps/.map");
+	 Display.setSize(36, 32);
+	 Display.setSpeed(10);
+    Digit first = new Zero(1,9);
+    Digit second = new One(7,9);
+    Digit third = new Five(13,9);
+    Digit fourth = new Two(19,9);
+    Digit fifth = new Three(25,9);
+    Digit sixth = new Two(31,9);
+    
+    first.display();
+    second.display();
+    third.display();
+    fourth.display();
+    fifth.display();
+    sixth.display();
 	 }
 	 
+ 
  }
